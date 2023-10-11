f9298817f937f7326799521bb8a6270e83ac2324
raokun <272728816@qq.com>
2023-10-11 11:01:22 +0800

    更新说明文档

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -41,7 +41,7 @@ export default defineUserConfig({
     // configure default theme
   theme: defaultTheme({
     logo: '/images/logo.png',
-    repo: 'TerraMours/TerraMours_Gpt_Api',
+    repo: 'TerraMours/TerraMours_Admin_Starter',
     docsDir: 'docs',
 
     // theme-level locales config
